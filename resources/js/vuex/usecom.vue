895fbff7ef9248a73df9ddde2605c0e4bc63eb63
Fariz Allawi Ghozi <farizallawi24@gmail.com>
2021-01-11 18:30:32 +0700

    new file:   resources/js/vuex/comA.vue 	new file:   resources/js/vuex/comB.vue 	new file:   resources/js/vuex/comC.vue 	new file:   resources/js/vuex/usecom.vue

---
+++ resources/js/vuex/usecom.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div class="content">
+            <div class="container-fluid">
+                <h1>I will show how all other components react to changes</h1>
+                <h2>The master components : {{ counter }}</h2>
+
+                <br>
+                <div>
+                    <comA></comA>
+                </div>
+                <br>
+                <div>
+                    <comB></comB>
+                </div>
+                <br>
+                <div>
+                    <comC></comC>
+                </div>
+
+                <br>
+                <Button type='info' @click="changeCounter">Change The state of the counter</Button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import comA from './comA.vue'
+import comB from './comB.vue'
+import comC from './comC.vue'
+import {mapGetters} from 'vuex'
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    computed: {
+        ...mapGetters({
+            'counter' : 'getCounter',
+        })
+    }, 
+
+    created() {
+        
+    },
+
+    methods: {
+        changeCounter() {
+            this.$store.dispatch('changeCounterAction' , 1)
+            this.$store.commit('changeTheCounter', 1)
+        },
+    },
+
+    components : {
+        comA,
+        comB,
+        comC
+    }
+}
+</script>
