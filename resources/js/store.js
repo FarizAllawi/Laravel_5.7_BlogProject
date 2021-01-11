3d3f0a56f1f1e723820e41f4fc72c0ef628622dc
Fariz Allawi Ghozi <farizallawi24@gmail.com>
2021-01-11 17:36:45 +0700

    new file:   public/uploads/1610356404.jpg 	new file:   public/uploads/1610356694.png 	new file:   public/uploads/1610356770.png 	new file:   public/uploads/1610356991.png 	new file:   public/uploads/1610357110.png 	new file:   public/uploads/1610357126.png 	new file:   resources/js/store.js

---
+++ resources/js/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+// import Vuex from 'vuex'
+export default new Vuex.Store({
+
+    state : {
+        counter : 1000,
+    },
+
+
+});
